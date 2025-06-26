32812c0e05d0c5d39cc73bef42b7f6b037c115e0
chopd37 <chopd37@gmail.com>
2025-06-26 17:15:24 +0900

    회원가입 구현 커밋

--- Ex01/vite.config.js
+++ Ex01/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+    server: {
+    proxy: {
+      // '/api'로 시작하는 요청은 전부 'http://localhost:3001'로 보내라는 규칙
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true, // CORS 오류를 방지하기 위해 출처(Origin)를 변경
+      }
+    }
+  }
 })
